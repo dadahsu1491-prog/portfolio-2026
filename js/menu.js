61002eae4e812359992f91c80d45d966c6c11321
dadahsu1491-prog <dadahsu1491@gmail.com>
2026-03-09 22:09:58 +0800

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,6 @@ let nav = document.querySelectorAll("nav")[0];
 let menu = document.querySelectorAll("menu")[0];
 let menuH = menu.scrollHeight;
 let boolNav = true;
-console.log(menuH);
 nav.addEventListener("click", function () {
     // menu.classList.toggle("none");
     if (boolNav) {
@@ -16,7 +15,7 @@ nav.addEventListener("click", function () {
     for (const bar of nav.children) {
         bar.classList.toggle("none");
     }
-});
+})
 for (const li of menu.children[0].children) {
     li.addEventListener("click", function () {
         // menu.classList.toggle("none");
@@ -29,7 +28,29 @@ for (const li of menu.children[0].children) {
         for (const bar of nav.children) {
             bar.classList.toggle("none");
         }
-    });
+    })
 }
 //YEAR 函數自動換年份
 document.getElementById("year").textContent = new Date().getFullYear();
+//gotop
+let goTop = document.createElement("a");
+goTop.id = "gotop";
+goTop.textContent = "TOP";
+goTop.href = "#";
+goTop.style.position = "fixed";
+goTop.style.width = "3em";
+goTop.style.hight = "3em";
+goTop.style.backgroundColor = "#264a9d94";
+goTop.style.color = "#fff";
+goTop.style.fontWeight = "700";
+goTop.style.textAlign = "center";
+goTop.style.lineHeight = "3em";
+goTop.style.fontSize = "0.9em";
+goTop.style.bottom = "1em";
+goTop.style.right = "1em";
+goTop.style.borderRadius = "50%";
+goTop.style.textDecoration = "none";
+goTop.style.zIndex = "200";
+
+document.getElementsByTagName("body")[0].appendChild(goTop);
+
